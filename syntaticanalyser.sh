204e5bb817efb9532b47e9ef12da0965f8d41f25
rafaelfrisch <rafaelcassolfrisch@gmail.com>
2022-11-11 17:45:20 -0300

    Fix .sh

--- syntaticanalyser.sh
+++ syntaticanalyser.sh
@@ -4,4 +4,5 @@ gcc -c *.c &&
 gcc -o cminus *.o -ll &&
 mkdir -p results &&
 ./cminus testfiles/mdc.c > results/mdc.txt &&
-./cminus testfiles/sort.c > results/sort.txt
+./cminus testfiles/sort.c > results/sort.txt &&
+./cminus testfiles/simple.c > results/simple.txt
