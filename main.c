51918864e4eb0bc5ec5069cd1a8d55bdbb2ba9ce
Erick Coelho <erickcoelho06@gmail.com>
2022-11-23 12:12:25 -0300

    cgen

--- main.c
+++ main.c
@@ -15,7 +15,7 @@
 /* set NO_CODE to TRUE to get a compiler that does not
  * generate code
  */
-#define NO_CODE TRUE
+#define NO_CODE FALSE
 
 #include "util.h"
 #if NO_PARSE
@@ -40,8 +40,8 @@ FILE * code;
 int EchoSource = FALSE;
 int TraceScan = FALSE;
 int TraceParse = FALSE;
-int TraceAnalyze = TRUE;
-int TraceCode = FALSE;
+int TraceAnalyze = FALSE;
+int TraceCode = TRUE;
 
 int Error = FALSE;
 
