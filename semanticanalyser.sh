ee63cd41190907a4a7bcb0709735e9fc674e99bb
Erick Coelho <erickcoelho06@gmail.com>
2022-11-24 14:54:13 -0300

    scrip update

--- semanticanalyser.sh
+++ semanticanalyser.sh
@@ -3,10 +3,11 @@ flex cminus.l &&
 gcc -c *.c &&
 gcc -o cminus *.o -ll &&
 mkdir -p results &&
-./cminus testfiles/mdc.c > results/mdc.txt &&
-./cminus testfiles/sort.c > results/sort.txt &&
-./cminus testfiles/simple.c > results/simple.txt &&
-./cminus testfiles/void.c > results/void.txt &&
-./cminus testfiles/nomain.c > results/nomain.txt &&
-./cminus testfiles/varvoid.c > results/varvoid.txt &&
+./cminus testfiles/erick.c > results/erick.txt &&
+#./cminus testfiles/mdc.c > results/mdc.txt &&
+#./cminus testfiles/sort.c > results/sort.txt &&
+#./cminus testfiles/simple.c > results/simple.txt &&
+#./cminus testfiles/void.c > results/void.txt &&
+#./cminus testfiles/nomain.c > results/nomain.txt &&
+#./cminus testfiles/varvoid.c > results/varvoid.txt &&
 ./clearfiles.sh
